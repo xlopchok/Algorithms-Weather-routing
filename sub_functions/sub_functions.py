0e0527070f5a8bc2d416e93181b9851919d0cc33
xlopchok <99295649+xlopchok@users.noreply.github.com>
2025-04-11 18:17:43 +0300

    Update sub_functions.py

--- sub_functions/sub_functions.py
+++ sub_functions/sub_functions.py
@@ -1,5 +1,7 @@
 import math
 
+from maps.Ocean_map import Ocean_map
+
 # Функция для вычисления расстояния с использованием формулы Haversine
 def haversine(lat1, lon1, lat2, lon2, unit='km'):
     # Радиус Земли в зависимости от единицы измерения
@@ -22,3 +24,28 @@ def haversine(lat1, lon1, lat2, lon2, unit='km'):
     # Расстояние
     distance = R * c
     return distance
+
+def smoothing_path(path, max_edge_dist = 300, Ocean_map = Ocean_map):
+    path = [point[::-1] for point in path] # Возвращаем маршрут в последовательность точек вида [lon lat]
+    new_path = []
+    curr_index = 0
+    while curr_index < len(path):
+        point1 = path[curr_index]
+        new_path.append(point1)
+        best_next_index = -1
+        best_dist = -1
+        for next_index in range(curr_index + 1, len(path)):
+            point2 = path[next_index]
+            line = LineString([point1, point2])
+            if Ocean_map.contains(line):
+                dist_coords = next_index - curr_index
+                distance = haversine(point1[1], point1[0], point2[1], point2[0])
+                if distance < max_edge_dist and dist_coords > best_dist:
+                    best_dist = dist_coords
+                    best_next_index = next_index
+        if best_next_index == -1:
+            curr_index += 1
+        else:
+            curr_index = best_next_index
+    new_path = [point[::-1] for point in new_path] # Возвращаем маршрут в последовательность точек вида [lat lon]
+    return new_path
